aac119aabdfd2c8f32401bbaaf566c4cf05119b9
dannyflatiron <dannyflatiron@gmail.com>
2020-03-05 17:20:21 -0500

    added code to enable sessions

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -5,7 +5,28 @@ class AthletesController < ApplicationController
         erb :login
     end
 
+    # this creates a session hash
+    # finds the user 
+    # logins them in 
+    # receives data from form to create a session hash
+    # redirects user to landing page
+    # authenticate the user
+    # authenticate password
+    post '/login' do
+        @athlete = Athlete.find_by(username: params[:username])
+        if @athlete.authenticate(params[:password])
+            session[:athlete_id] = @athlete.id
+            # use double quotations for line 20 to work
+            redirect "/athletes/#{@athlete.id}"
+        else
+        end
+        erb :login
+    end
+
     get '/signup' do
     end
 
+    get '/athletes/:id' do
+        "Athlete's page"
+    end
 end
