d10c8458f9430148fae6d6edbfe76fe7d801f732
dannyflatiron <dannyflatiron@gmail.com>
2020-03-11 12:04:37 -0400

    added nav buttons

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,12 @@ ActiveRecord::Schema.define(version: 20200304012339) do
 
   create_table "athletes", force: :cascade do |t|
     t.string "username"
-    # gem bcrypt hashes and salts password through a hashing algorithm 
-    # should use macro 'has_secure_password' and ActiveRecord method 'authenticate'
-    # prevents from having strings in the database
     t.string "password_digest"
   end
 
   create_table "exercises", force: :cascade do |t|
     t.string  "name"
     t.date    "date_performed"
-    # change dataype for max_lift to string or figure out how to add lbs or kg to show view
     t.integer "max_lift"
     t.integer "athlete_id"
   end
