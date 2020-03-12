8d63c6a3a65935a0ac94b573ae2b5b6427a734fe
dannyflatiron <dannyflatiron@gmail.com>
2020-03-12 17:48:31 -0400

    attempted to use validatation presence true but it didn't work so its been commented out

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,9 @@ class Exercise < ActiveRecord::Base
 
     # a belongs_to macro means there must be a foreign id key in the objects data table
     belongs_to :athlete
+
+
+    # does this work for this class/
+    # validates :name, presence: true
+    # validates :max_lift, presence: true
 end
