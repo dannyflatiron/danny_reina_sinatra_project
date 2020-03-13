ba9c01ddc8584748eae9bc9e93ac7f4aa1885e50
dannyflatiron <dannyflatiron@gmail.com>
2020-03-13 17:21:35 -0400

    no changes made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < Sinatra::Base
 
     def exercise_entry
       @exercise = Exercise.find_by_id(params[:id])
+      
       # @exercise = current_user.exercises.find_by(params[:id])
     end
 
